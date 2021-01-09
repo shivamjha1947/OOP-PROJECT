0bf22f58cc8a336a8d403f9bb04a213c2a78b860
shivam jha <shivam36_soe@jnu.ac.in>
2021-01-09 14:27:26 +0530

    Add files via upload

---
+++ TETRIS/Tetris.h
@@ -0,0 +1,10 @@
+#pragma once
+#include <wx/wx.h>
+
+class Tetris : public wxFrame
+{
+public:
+    Tetris(const wxString& title);
+
+};
+
---
+++ TETRIS/main.h
@@ -0,0 +1,10 @@
+#pragma once
+#include <wx/wx.h>
+
+class MyApp : public wxApp
+{
+public:
+    virtual bool OnInit();
+
+};
+
